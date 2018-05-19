0088d5affbf28b7ccb7da7bef8b03fb76132768a
Tomasz Paterka <tomekpaterka@gmail.com>
2018-05-19 22:54:32 +0200

    Add function that shows countries details

---
+++ src/presentational/country-details.component.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const CountryDetails = (props) => (
+	<div className="country-wrapper">
+		<header>
+			<img className="country-photo" src={props.country.imageUrl} alt="country photo" />
+		</header>
+		<div className="country-info">
+			<h1>{props.country.name}</h1>
+			<h2>Continent: {props.country.continent}</h2>
+			
+			<div className="info">
+				<div>
+					<span>{props.country.populace}</span>
+					<span>Population[mln]</span>
+				</div>
+				<div>
+					<span>{props.country.capital}</span>
+					<span>Capital</span>
+				</div>
+				<div>
+					<span>{props.country.currency}</span>
+					<span>Currency</span>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default CountryDetails;
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,15 @@ import Home from './presentational/home.component.js';
 import Contact from './presentational/contact.component.js';
 import NotFound from './presentational/not-found.component.js';
 import CountryFlagContainer from './containers/flag-container.component.js';
+import CountryDetailsContainer from './containers/country-detail-container.component.js';
 
 export default (
 	<Route path='/' component={Navigation}>
 		<IndexRoute component={Home}></IndexRoute>
-		<Route path='countries' component={CountryFlagContainer}></Route>
+		<Route path='countries'>
+			<IndexRoute component={CountryFlagContainer}></IndexRoute>
+			<Route path='country/:id' component={CountryDetailsContainer}></Route>
+		</Route>
 		<Route path='contact' component={Contact}></Route>
 		<Route path='*' component={NotFound}></Route>
 	</Route>
