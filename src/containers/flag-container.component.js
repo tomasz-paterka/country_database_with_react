e13a8866b5bd5363910a2aa6ab41776a93a5d625
Tomasz Paterka <tomekpaterka@gmail.com>
2018-05-19 23:38:59 +0200

    add function deleting selected country

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CountryFlagList from '../presentational/flag-list.component.js';
-import { getCountries, searchCountries } from '../actions/actions-countries.js';
+import { getCountries, searchCountries, deleteCountry } from '../actions/actions-countries.js';
 
 class CountryFlagContainer extends Component {
 	constructor(props) {
@@ -17,13 +17,17 @@ class CountryFlagContainer extends Component {
 		this.props.dispatch(searchCountries(event.target.value));
 	}
 	
+	deleteCountry(id) {
+		this.props.dispatch(deleteCountry(id));
+	}
+	
 	render() {
 		return (
 			<div>
 				<div className="search text-center">
 					<input type="text" onChange={this.search.bind(this)}/>
 				</div>
-				<CountryFlagList countries={this.props.visibleCountries}/>
+				<CountryFlagList countries={this.props.visibleCountries} deleteCountry={this.deleteCountry.bind(this)} />
 			</div>
 		)
 	}
--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,4 +1,4 @@
-import {GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES } from '../actions/actions-countries';
+import {GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY } from '../actions/actions-countries';
 import countriesData from '../data/countries.json';
 
 const initialState = {
@@ -19,6 +19,11 @@ const countriesReducer = function (state = initialState, action) {
 		case SEARCH_COUNTRIES:
 			const foundCountries = state.countries.filter(country => country.name.toLowerCase().includes(action.searchText.toLowerCase()));
 			return Object.assign({}, state, {visibleCountries: foundCountries});
+		
+		case DELETE_COUNTRY:
+			const notDeletedCountries = state.countries.filter(country => country.id != action.id);
+			const notDeletedVisibleCountries = state.visibleCountries.filter(country => country.id != action.id);
+			return Object.assign({}, state, {countries: notDeletedCountries, visibleCountries: notDeletedVisibleCountries});
 	}
 	return state;
 };
