0901effb25f8dc828752a403f5f691607cc2d399
Tomasz Paterka <tomekpaterka@gmail.com>
2018-05-19 16:54:43 +0200

    Add function to list all countries

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import Navigation from './presentational/navigation.component.js';
 import Home from './presentational/home.component.js';
 import Contact from './presentational/contact.component.js';
 import NotFound from './presentational/not-found.component.js';
+import CountryFlagContainer from './containers/flag-container.component.js';
 
 export default (
 	<Route path='/' component={Navigation}>
 		<IndexRoute component={Home}></IndexRoute>
+		<Route path='countries' component={CountryFlagContainer}></Route>
 		<Route path='contact' component={Contact}></Route>
 		<Route path='*' component={NotFound}></Route>
 	</Route>
